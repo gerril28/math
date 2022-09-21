d11123c2a2d0310bea7ced1ec931440bd3bba85f
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-21 09:19:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    number = Math.trunc(23.9)
+    basic.showNumber(number)
+})
+let number = 0
+basic.showIcon(IconNames.EigthNote)
+basic.forever(function () {
+	
+})
